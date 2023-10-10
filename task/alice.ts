43b72181862d03afa7db9a1b8e04d9be7eb0d079
Mimiko Phonowell <phonowell@gmail.com>
2023-10-10 15:31:06 +0800

    refactor: updated

--- task/alice.ts
+++ task/alice.ts
@@ -6,8 +6,11 @@ import { argv, getBasename, glob, prompt } from '../src'
 
 type FnAsync = <T>() => Promise<T>
 
-// function
-
+/**
+ * Prompts the user to select a task from a list of options.
+ * @param list - The list of options to choose from.
+ * @returns A Promise that resolves to the selected task as a string.
+ */
 const ask = async (list: string[]): Promise<string> => {
   const answer = await prompt({
     id: 'default-task',
@@ -20,19 +23,26 @@ const ask = async (list: string[]): Promise<string> => {
   return answer
 }
 
-const load = async (): Promise<string[]> => {
+/**
+ * Finds all files in the `./task` directory with a `.js` or `.ts` extension, except for `.d.ts` files.
+ * @returns A Promise that resolves to an array of file names as strings.
+ */
+const load = async () => {
   const listSource = await glob(['./task/*.js', './task/*.ts', '!*.d.ts'])
 
   const listResult = listSource.map(source => {
-    const basename = getBasename(source)
+    const basename: string = getBasename(source)
     return basename === 'alice' ? '' : basename
   })
 
   return compact(listResult)
 }
 
+/**
+ * The main function that runs the program.
+ */
 const main = async () => {
-  const task = argv()._[0]
+  const task: string = argv()._[0]
     ? argv()._[0].toString()
     : await (async () => ask(await load()))()
 
@@ -40,10 +50,14 @@ const main = async () => {
   await run(task)
 }
 
+/**
+ * Imports the file corresponding to the selected task and calls the default exported function.
+ * @param task - The name of the task to run.
+ */
 const run = async (task: string) => {
   const [source] = await glob([`./task/${task}.js`, `./task/${task}.ts`])
 
-  const fn = ((await import(source)) as { default: FnAsync }).default
+  const fn: FnAsync = ((await import(source)) as { default: FnAsync }).default
   await fn()
 }
 
